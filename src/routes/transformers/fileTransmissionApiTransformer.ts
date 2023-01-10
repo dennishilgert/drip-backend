44e51644ed8808e1101316ae978af0d6fd920dca
Dennis Hilgert <d.hilgert@laserhub.com>
2023-01-10 20:25:03 +0100

    Fixed wrong interface reference

--- src/routes/transformers/fileTransmissionApiTransformer.ts
+++ src/routes/transformers/fileTransmissionApiTransformer.ts
@@ -3,19 +3,19 @@ import * as TransmissionsModule from '../../modules/transmissions'
 import _ from 'lodash'
 
 class FileTransmissionApiTransformer extends BaseTransformer {
-  private readonly fileTransmission: TransmissionsModule.types.IFileSending
+  private readonly fileTransmission: TransmissionsModule.types.IFileTransmission
 
-  constructor(fileTransmission: TransmissionsModule.types.IFileSending) {
+  constructor(fileTransmission: TransmissionsModule.types.IFileTransmission) {
     super()
     this.fileTransmission = fileTransmission
   }
 
-  transform(): TransmissionsModule.types.ITransformedFileSending {
+  transform(): TransmissionsModule.types.ITransformedFileTransmission {
     const pickProperties: Array<string> = ['uuid', 'fromName', 'fileOriginalName', 'fileMimeType']
-    const transformedFileTransmission: TransmissionsModule.types.ITransformedFileSending = _.pick(
+    const transformedFileTransmission: TransmissionsModule.types.ITransformedFileTransmission = _.pick(
       this.fileTransmission,
       pickProperties
-    ) as TransmissionsModule.types.ITransformedFileSending
+    ) as TransmissionsModule.types.ITransformedFileTransmission
     return transformedFileTransmission
   }
 }
