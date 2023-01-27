117042f9ce86bff3ced857b07ab06ce65186565f
Dennis Hilgert <d.hilgert@laserhub.com>
2023-01-27 09:37:08 +0100

    Define database connection params in separate constants

--- src/modules/databaseModels.ts
+++ src/modules/databaseModels.ts
@@ -6,18 +6,17 @@ import { asNumber, asString } from '../common/helpers/dataHelper'
 const database: any = {}
 
 const databaseName: string = asString(process.env.DB_DATABASE)
+const host: string = asString(process.env.DB_HOST)
+const port: number = asNumber(process.env.DB_PORT)
 const username: string = asString(process.env.DB_USERNAME)
 const password: string = asString(process.env.DB_PASSWORD)
 const maxConnectionsPerPool: number = asNumber(process.env.DB_MAX_CONNECTIONS_PER_POOL)
-
-logger.debug('database', databaseName)
-logger.debug('username', username)
-logger.debug('maxConnectionsPerPool', maxConnectionsPerPool)
+const dbSlowQueryThreshold: number = asNumber(process.env.DB_SLOW_QUERY_THRESHOLD)
 
 const sequelize: Sequelize = new Sequelize(databaseName, username, password, {
   dialect: 'mariadb',
-  host: asString(process.env.DB_HOST),
-  port: asNumber(process.env.DB_PORT),
+  host,
+  port,
   pool: {
     max: maxConnectionsPerPool,
     min: 2,
@@ -26,7 +25,7 @@ const sequelize: Sequelize = new Sequelize(databaseName, username, password, {
   },
   benchmark: true, // passes the query execution time as second arg to logging method
   logging: (queryString, execTimeMs) => {
-    if (asNumber(execTimeMs) > asNumber(process.env.DB_SLOW_QUERY_THRESHOLD)) {
+    if (asNumber(execTimeMs) > dbSlowQueryThreshold) {
       logger.warn(`Slow sequelize query ${execTimeMs}ms`, {
         queryString
       })
